5dfb3ac3781b3fbdacecbbec7acc5c416420bfb5
Mateo Álvarez <mateo@netprogroup.com>
2024-12-09 02:22:00 -0500

    feat: update auth

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestaurantController;
 
 Route::get('/', function () {
-    return "Hola mundoooo";
+    return "Mateo Alvarez Lebrum";
 });
 
-Route::resource('restaurants', RestaurantController::class);
+Route::post('/signup', [AuthController::class, 'register']);
+Route::post('/signin', [AuthController::class, 'login']);
+
+Route::get('/restaurants', [RestaurantController::class, 'index']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/restaurants', [RestaurantController::class, 'store']);
+    Route::get('/restaurants/{id}', [RestaurantController::class, 'show']);
+    Route::put('/restaurants/{id}', [RestaurantController::class, 'update']);
+    Route::delete('/restaurants/{id}', [RestaurantController::class, 'destroy']);
+});
